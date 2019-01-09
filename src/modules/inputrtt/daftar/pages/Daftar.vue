4e87e602dc926a5c025f616e5ffa6428d00cda8a
novandri511 <novandri511@gmail.com>
2019-01-09 15:04:44 +0700

    UPDATE TABLE

--- src/modules/inputrtt/daftar/pages/Daftar.vue
+++ src/modules/inputrtt/daftar/pages/Daftar.vue
@@ -1,10 +1,106 @@
 <template>
-    <div id="container">
-        <table border="1">
+<div>
+<div class="pb-3">
+    <v-container grid-list-xs fill-height>
+        <v-layout>
+            <v-flex xs12>
+                <v-card class="pb-3">
+                    <v-card-title class="elevation-1 font-weight-bold">
+                        INPUT ANGGARAN KEGIATAN R1,R2
+                    </v-card-title>
+                        <v-card-text class="py-0">
+                            <v-container grid-list-md class="pa-0">
+                                <v-layout row wrap>
+                                    <v-flex md2 sm6>
+                                        <v-text-field
+                                            append-icon="search"
+                                            label="TAHUN"
+                                            single-line
+                                            hide-details
+                                            @input="q_code"
+                                        ></v-text-field>
+                                    </v-flex>
+                                    <v-flex md2 sm6>
+                                        <v-text-field
+                                            append-icon="search"
+                                            label="KHG"
+                                            single-line
+                                            hide-details
+                                            @input="q_code"
+                                        ></v-text-field>
+                                    </v-flex>
+                                    <v-flex md2 sm6>
+                                        <v-text-field
+                                            append-icon="search"
+                                            label="KECAMATAN"
+                                            single-line
+                                            hide-details
+                                            @input="q_code"
+                                        ></v-text-field>
+                                    </v-flex>
+                                    <v-flex md2 sm6>
+                                        <v-text-field
+                                            append-icon="search"
+                                            label="JENIS KEGIATAN"
+                                            single-line
+                                            hide-details
+                                            @input="q_code"
+                                        ></v-text-field>
+                                    </v-flex>
+                                    <v-btn block color="primary" outline @click="load(1)">Search</v-btn>
+                                </v-layout>
+                            </v-container>
+                            <v-container grid-list-md class="pa-0">
+                                <v-layout row wrap>
+                                    <v-flex md2 sm6>
+                                        <v-text-field
+                                            append-icon="search"
+                                            label="PROVINSI"
+                                            single-line
+                                            hide-details
+                                            @input="q_code"
+                                        ></v-text-field>
+                                    </v-flex>
+                                    <v-flex md2 sm6>
+                                        <v-text-field
+                                            append-icon="search"
+                                            label="KABUPATEN"
+                                            single-line
+                                            hide-details
+                                            @input="q_code"
+                                        ></v-text-field>
+                                    </v-flex>
+                                    <v-flex md2 sm6>
+                                        <v-text-field
+                                            append-icon="search"
+                                            label="DESA"
+                                            single-line
+                                            hide-details
+                                            @input="q_code"
+                                        ></v-text-field>
+                                    </v-flex>
+                                                                      
+                                </v-layout>
+                            </v-container>
+
+                        </v-card-text>
+
+                </v-card>
+ <v-card-text>
+                        DAFTAR TINDAKAN RTT KHG ... Provinsi ... Tahun ... 
+                    </v-card-text>            </v-flex>
+
+        </v-layout>
+       
+    </v-container>
+</div>
+
+<div class="container">
+    <h1> INPUT ANGGARAN KEGIATAN R1, R2</h1>
+        <table border="5">
             <template v-for="row in storage.rows">
-            
-                <h1> Tabel Report Biaya </h1>
-                    <table border  height="400px" width="50%">
+                
+                    <table border  height="300px" width="100%">
                         <tr>
                             <th rowspan="2" bgcolor="#d6d8db" style="border-bottom:3px solid black;">NO</th>
                             <th rowspan="2" bgcolor="#d6d8db" style="border-bottom:3px solid black;">KABUPATEN</th>
@@ -62,6 +158,7 @@
             </template>
         </table>
     </div>
+</div>
 </template>
 
 
