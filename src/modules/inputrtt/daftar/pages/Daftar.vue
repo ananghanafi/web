bd3a5c69a2122eddc1fd5bd410ad938b26f3d3a2
novandri511 <novandri511@gmail.com>
2019-01-09 13:47:54 +0700

    update tabel

--- src/modules/inputrtt/daftar/pages/Daftar.vue
+++ src/modules/inputrtt/daftar/pages/Daftar.vue
@@ -2,14 +2,63 @@
     <div id="container">
         <table border="1">
             <template v-for="row in storage.rows">
-                <tr>
-                    <td rowspan="2"><input type="checkbox"></td>
-                    <td rowspan="2">{{row.sn}}</td>
-                    <td>{{row.source}}</td>
-                </tr>
-                <tr>
-                    <td>{{row.pair.source}}</td>
-                </tr>
+            
+                <h1> Tabel Report Biaya </h1>
+                    <table border  height="400px" width="50%">
+                        <tr>
+                            <th rowspan="2" bgcolor="#d6d8db" style="border-bottom:3px solid black;">NO</th>
+                            <th rowspan="2" bgcolor="#d6d8db" style="border-bottom:3px solid black;">KABUPATEN</th>
+                            <th rowspan="2" bgcolor="#d6d8db" style="border-bottom:3px solid black;">URPG</th>
+                            <th colspan="3" bgcolor="#d6d8db">LOKASI & LUAS AREAL RESTORASI</th>
+                            <th colspan="3" bgcolor="#d6d8db">      SUMUR BOR       </th>
+                            <th colspan="2" bgcolor="#d6d8db">BIAYA</th>
+                        </tr>
+                        <tr>
+                           <th rowspan="1" bgcolor="#d6d8db" style="border-bottom:3px solid black;">KECAMATAN</th>
+                           <th rowspan="1" bgcolor="#d6d8db" style="border-bottom:3px solid black;">DESA</th>
+                           <th rowspan="1" bgcolor="#d6d8db" style="border-bottom:3px solid black;">LUAS AREAL RESTORASI(Hektar) </th>
+                           <th rowspan="1" bgcolor="#d6d8db" style="border-bottom:3px solid black;">INDIKATIF FUNGSI BUDIDAYA E.G(Unit)</th>
+                           <th rowspan="1" bgcolor="#d6d8db" style="border-bottom:3px solid black;">INDIKATIF FUNGSI BUDIDAYA E.G (Unit)</th>
+                           <th rowspan="1" bgcolor="#d6d8db" style="border-bottom:3px solid black;">TOTAL(Unit)</th>
+                           <th rowspan="1" bgcolor="#d6d8db" style="border-bottom:3px solid black;">SATUAN(Rp)</th>
+                           <th rowspan="1" bgcolor="#d6d8db" style="border-bottom:3px solid black;">TOTAL(Rp)</th>
+                        </tr>
+                        <tr>
+                         <td>...</td>
+                         <td>...</td>
+                         <td>...</td>
+                         <td>...</td>
+                         <td>...</td>
+                         <td>...</td>
+                         <td>...</td>
+                         <td>...</td>
+                         <td>...</td>
+                         <td>...</td>
+                         <td>...</td>
+                        </tr>
+                        <tr>
+                         <td>...</td>
+                         <td>...</td>
+                         <td>...</td>
+                         <td>...</td>
+                         <td>...</td>
+                         <td>...</td>
+                         <td>...</td>
+                         <td>...</td>
+                         <td>...</td>
+                         <td>...</td>
+                         <td>...</td>
+                        </tr>   
+                        <tr>
+                        <td colspan="5">TOTAL   </td>
+                        <td colspan="1">SUM</td>
+                        <td colspan="1">SUM   </td>
+                        <td colspan="1">SUM   </td>
+                        <td colspan="1">SUM   </td>
+                        <td colspan="1">SUM   </td>
+                        <td colspan="1">SUM   </td>
+                        </tr>
+                    </table>
             </template>
         </table>
     </div>
@@ -24,8 +73,9 @@ export default {
             storage: {
                 rows: [
                     {sn: 111, source: 'EK', pair: {source: 'VLT', in: 100}},
-                    {sn: 222, source: 'EK', pair: {source: 'VLT', in: 200}}
+                    
                 ]
+                
             }
         }
     }
