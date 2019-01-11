ed8e35769f678f71819979ced38ace7a47d37bdf
novandri511 <novandri511@gmail.com>
2019-01-12 01:42:40 +0700

    update tabel

--- src/modules/sekatkanal/daftar/pages/Daftar.vue
+++ src/modules/sekatkanal/daftar/pages/Daftar.vue
@@ -62,17 +62,129 @@
                             </v-container>
                             
                         </v-card-text>
+                            <v-card-text>
+                            TINDAKAN RTT    TAHUN ...... Provinsi ...... KHG ...... 
+                            </v-card-text>
+                    <template>
+  <v-data-table
+    :headers="headers"
+    :items="desserts"
+    :pagination.sync="pagination"
+    item-key="name"
+    class="elevation-1"
+  >
+            <template slot="headers" slot-scope="props">
+                    <tr>
+                      <th
+                        v-for="header in props.headers"
+                        :key="header.text"
+                        :rowspan="header.rowspan"
+                        :class="['column sortable', pagination.descending ? 'desc' : 'asc', header.value === pagination.sortBy ? 'active' : '']"
+                        @click="changeSort(header.value)"
+                      >
+                        <v-icon small>arrow_upward</v-icon>
+                        {{ header.text }}
+                      </th>
 
+                      <th
+                        v-for="header in headers3"
+                        :key="header.text"
+                        :rowspan="header.rowspan"
+                        :class="['column sortable', pagination.descending ? 'desc' : 'asc', header.value === pagination.sortBy ? 'active' : '']"
+                        @click="changeSort(header.value)"
+                      >
+                        <v-icon small>arrow_upward</v-icon>
+                        {{ header.text }}
+                      </th>
+                    
+
+                    <th
+                        v-for="header in headers4"
+                        :key="header.text"
+                        :rowspan="header.rowspan"
+                        :class="['column sortable', pagination.descending ? 'desc' : 'asc', header.value === pagination.sortBy ? 'active' : '']"
+                        @click="changeSort(header.value)"
+                      >
+                        <v-icon small>arrow_upward</v-icon>
+                        {{ header.text }}
+                      </th>
+
+                    <th
+                        v-for="header in headers5"
+                        :key="header.text"
+                        :rowspan="header.rowspan"
+                        :class="['column sortable', pagination.descending ? 'desc' : 'asc', header.value === pagination.sortBy ? 'active' : '']"
+                        @click="changeSort(header.value)"
+                      >
+                        <v-icon small>arrow_upward</v-icon>
+                        {{ header.text }}
+                      </th>
+                    
+
+                    <th
+                        v-for="header in headers6"
+                        :key="header.text"
+                        :rowspan="header.rowspan"
+                        :class="['column sortable', pagination.descending ? 'desc' : 'asc', header.value === pagination.sortBy ? 'active' : '']"
+                        @click="changeSort(header.value)"
+                      >
+                        <v-icon small>arrow_upward</v-icon>
+                        {{ header.text }}
+                      </th>
+                   
+
+                    <th
+                        v-for="header in headers7"
+                        :key="header.text"
+                        :colspan="header.colspan"
+                        :class="['column sortable', pagination.descending ? 'desc' : 'asc', header.value === pagination.sortBy ? 'active' : '']"
+                        @click="changeSort(header.value)"
+                      >
+                        <v-icon small>arrow_upward</v-icon>
+                        {{ header.text }}
+                      </th>
+                    </tr>
+                    </tr>
+
+
+                    <tr>      
+                        <th
+                        v-for="header in headers8"
+                        :key="header.text"
+                        :class="['column sortable', pagination.descending ? 'desc' : 'asc', header.value === pagination.sortBy ? 'active' : '']"
+                        @click="changeSort(header.value)"
+                      >
+                        <v-icon small>arrow_upward</v-icon>
+                        {{ header.text }}
+                      </th>
+                    </tr>
+                  </template>
+                <template slot="items" slot-scope="props">
+    
+                <tr>
+              <td rowspan="2">{{ props.item.name }}</td>
+              <td rowspan="2" class="text-xs-right">{{ props.item.calories }}</td>
+              <td class="text-xs-right">{{ props.item.fat }}</td>
+              <td class="text-xs-right">{{ props.item.carbs }}</td>
+                </tr>
+                <tr>
+              <td rowspan="1"class="text-xs-right">{{ props.item.satuan }}</td>
+              <td rowspan="1"class="text-xs-right">{{ props.item.total}}</td>
+                </tr>
+    
+            </template>
+        </v-data-table>
+</template>
                 </v-card>
-                    <v-card-text>
-                        TINDAKAN RTT    TAHUN ...... Provinsi ...... KHG ...... 
-                    </v-card-text>            
+                                
             </v-flex>
 
         </v-layout>
        
     </v-container>
 </div>
+
+
 
 <div class="container">
     
@@ -142,10 +254,176 @@ export default {
                     
                 ]
                 
+            },
+          headers: [
+          {
+            text: 'No',
+            align: 'left',
+            sortable: false,
+            value: 'name',
+            rowspan: '2',
+          },
+       
+         
+        ],
+        
+        headers3: [
+        {
+            text: 'Kabupaten',
+            align: 'left',
+            sortable: 'false',
+            value: 'name',
+            rowspan: '2',
             }
-        }
+        ],
+        headers4: [
+        {
+            text: 'UPRG',
+            align: 'left',
+            sortable: 'false',
+            value: 'name',
+            rowspan: '2',
+            }
+        ],
+        headers5: [
+        {
+            text: 'Jumlah (unit)',
+            align: 'left',
+            sortable: 'false',
+            value: 'name',
+            rowspan: '2',
+            }
+        ],
+        headers6: [
+        {
+            text: 'Luas Areal Restorasi(Hektar)',
+            align: 'left',
+            sortable: 'false',
+            value: 'name',
+            rowspan: '2',
+            }
+        ],
+        headers7: [
+        {
+            text: 'Biaya',
+            align: 'left',
+            sortable: 'false',
+            value: 'name',
+            colspan: '2',
+            }
+        ],
+        headers8:[
+             { text: 'Satuan (Rp)', value: 'biayasatuan' },
+             { text: 'Total (Rp)', value: 'biayatotal' }
+        ],
+        sekatkanal:[
+        {
+          value: false,
+          No: '',
+          Kabupaten: '',
+          UPRG: '',
+          Jumlah: '',
+          LuasArealRestorasi: '',
+          BiayaSatuan: '',
+          BiayaTotal: '',
+        }],
+        desserts: [
+          {
+            value: false,
+            name: 'Frozen Yogurt',
+            calories: 159,
+            fat: 6.0,
+            carbs: 24,
+            protein: 4.0,
+            iron: '1%'
+          },
+          {
+            value: false,
+            name: 'Ice cream sandwich',
+            calories: 237,
+            fat: 9.0,
+            carbs: 37,
+            protein: 4.3,
+            iron: '1%'
+          },
+          {
+            value: false,
+            name: 'Eclair',
+            calories: 262,
+            fat: 16.0,
+            carbs: 23,
+            protein: 6.0,
+            iron: '7%'
+          },
+          {
+            value: false,
+            name: 'Cupcake',
+            calories: 305,
+            fat: 3.7,
+            carbs: 67,
+            protein: 4.3,
+            iron: '8%'
+          },
+          {
+            value: false,
+            name: 'Gingerbread',
+            calories: 356,
+            fat: 16.0,
+            carbs: 49,
+            protein: 3.9,
+            iron: '16%'
+          },
+          {
+            value: false,
+            name: 'Jelly bean',
+            calories: 375,
+            fat: 0.0,
+            carbs: 94,
+            protein: 0.0,
+            iron: '0%'
+          },
+          {
+            value: false,
+            name: 'Lollipop',
+            calories: 392,
+            fat: 0.2,
+            carbs: 98,
+            protein: 0,
+            iron: '2%'
+          },
+          {
+            value: false,
+            name: 'Honeycomb',
+            calories: 408,
+            fat: 3.2,
+            carbs: 87,
+            protein: 6.5,
+            iron: '45%'
+          },
+          {
+            value: false,
+            name: 'Donut',
+            calories: 452,
+            fat: 25.0,
+            carbs: 51,
+            protein: 4.9,
+            iron: '22%'
+          },
+          {
+            value: false,
+            name: 'KitKat',
+            calories: 518,
+            fat: 26.0,
+            carbs: 65,
+            protein: 7,
+            iron: '6%'
+          }
+        ]
+      }
+        
     }
 }
+
 </script>
 
 // styling for a template
